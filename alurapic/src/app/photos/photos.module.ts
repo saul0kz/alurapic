1f211d5844dbec9970cc69797ada8cf184bec2c0
Saulo Monteiro <saulo@mercado79.com>
2019-10-18 20:02:35 -0300

    adicionando bootstrap

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -6,10 +6,11 @@ import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 
 @NgModule({
-    declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionPipe],
+    declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionPipe, LoadButtonComponent],
     exports: [PhotoComponent],
     imports: [HttpClientModule, CommonModule],
 })
