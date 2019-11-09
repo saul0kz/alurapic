b2253de33ddbed1df74ae7eab41a04dcc8055576
Saulo Monteiro <saulo@mercado79.com>
2019-11-09 11:10:01 -0300

    fazendo login com Authservice

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -10,13 +11,29 @@ export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required],
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(
+        () => window.alert("Autenticado"),
+        err => {
+         window.alert(err.message);
+          this.loginForm.reset();
+        }
+      );
+
+
   }
 
 }
